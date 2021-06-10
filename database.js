8bb76b74d505a2fc2c72a71e7d4a5f224e29141a
mercyy-maker <lelagom@spu.edu>
2021-06-10 15:21:03 -0700

    fasdfa

--- database.js
+++ database.js
@@ -2,7 +2,7 @@
 const mongoose = require('mongoose');
 
 // Update with your own Database URI
-const mongoDB = process.env.MONGODB_URI || 'mongodb+srv://Mercy_ad:Newpass2!@cluster0.fldqj.mongodb.net/books?retryWrites=true&w=majority';
+const mongoDB = process.env.MONGODB_URI || 'mongodb+srv://Mercy_ad:Newpass@cluster0.fldqj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 
 mongoose
     .connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true})
--- routes/books.js
+++ routes/books.js
@@ -1,9 +1,153 @@
-var express = require('express');
-var router = express.Router();
+// For the routes
+let express = require('express');
+let router = express.Router();
+// For the Data Model
+let BookSchema = require('../models/books');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+
+function HandleError(response, reason, message, code){
+  console.log('ERROR: ' + reason);
+  response.status(code || 500).json({"error:": message});
+}
+
+
+
+router.post('/', (request, response, next) => {
+  let newBook = request.body;
+  //console.log(request.body);
+  if (!newBook.name || !newBook.author || !newBook.isbn ||!newBook.price ){
+    HandleError(response, 'Missing Info', 'Form data missing', 500);
+  }
+  const ISBN = require( 'isbn-validate' );
+  if(!ISBN.Validate(newBook.isbn)){
+    HandleError(response, 'NO ISBN VALIDATION ', 'invalid isbn', 500);
+  }
+  else{
+    let book = new BookSchema({
+      name: newBook.name,
+      author: newBook.author,
+      isbn: newBook.isbn,
+      price: newBook.price,
+    });
+    book.save((error) => {
+      if (error){
+        response.send({"error": error});
+      }else{
+        response.send({"id": book.id});
+      }
+    });
+  }
 });
+
+
+router.get('/', (request, response, next) => {
+  let name = request.query['author'];
+  if (name){
+    BookSchema
+        .find({"author": name})
+        .exec( (error, books) => {
+          if (error){
+            response.send({"error": error});
+          }else{
+            response.send(books);
+          }
+        });
+  }else{
+    BookSchema
+        .find()
+        .exec( (error, books) => {
+          if (error){
+            response.send({"error": error});
+          }else{
+            response.send(books);
+          }
+        });
+  }
+
+} );
+router.get('/', (request, response, next) => {
+  let isbn = request.query['isbn'];
+  if (isbn) {
+    BookSchema
+        .find({"isbn": isbn})
+        .exec((error, books) => {
+          if (error) {
+            response.send({"error": error});
+          } else {
+            response.send(books);
+          }
+        });
+  } else {
+    BookSchema
+        .find()
+        .exec((error, books) => {
+          if (error) {
+            response.send({"error": error});
+          } else {
+            response.send(books);
+          }
+        });
+  }
+});
+
+
+router.get('/:isbn', (request, response, next) =>{
+  BookSchema
+      .findOne({"isbn": request.params.isbn}, (error, result) =>{
+        if (error) {
+          response.status(500).send(error);
+        }
+        if (result){
+          response.send(result);
+        }else{
+          response.status(404).send({"isbn": request.params.isbn, "error":  "Not Found"});
+        }
+
+      });
+});
+
+router.patch('/:isbn', (request, response, next) =>{
+  BookSchema
+      .findOne({"isbn": request.params.isbn}, (error, result) =>{
+        if (error) {
+          response.status(500).send(error);
+        }else if (result){
+          if (request.body.isbn){
+            delete request.body.isbn;
+          }
+          for (let field in request.body){
+            result[field] = request.body[field];
+          }
+          result.save((error, book)=>{
+            if (error){
+              response.status(500).send(error);
+            }
+            response.send(book);
+          });
+        }else{
+          response.status(404).send({"isbn": request.params.isbn, "error":  "Not Found"});
+        }
+
+      });
+});
+
+router.delete('/:isbn', (request, response, next) =>{
+  BookSchema
+      .findOne({"isbn": request.params.isbn}, (error, result) =>{
+        if (error) {
+          response.status(500).send(error);
+        }else if (result){
+          result.remove((error)=>{
+            if (error){
+              response.status(500).send(error);
+            }
+            response.send({"deletedISBN": request.params.isbn});
+          });
+        }else{
+          response.status(404).send({"isbn": request.params.isbn, "error":  "Not Found"});
+        }
+      });
+});
+
 
 module.exports = router;
