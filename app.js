f4a9a9db41df334b4f634550575ae5e4e04d63c3
mercyy-maker <lelagom@spu.edu>
2021-06-13 20:05:06 -0700

    app js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors');
 
 var db = require('./database');
 var indexRouter = require('./routes/index');
@@ -21,6 +22,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(cors());
 app.use((request, response, next) => {
   response.header('Access-Control-Allow-Origin', '*');
   next();
